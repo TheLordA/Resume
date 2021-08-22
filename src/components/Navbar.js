f6b1595d1d93282779d34b08bb83ad69bc77a7e1
TheLordA <45141652+TheLordA@users.noreply.github.com>
2021-08-22 16:07:58 +0100

    Fix Navbar Display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import { AiFillStar, AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from "react-icons/ai";
-
 import { CgFileDocument } from "react-icons/cg";
 
 const NavBar = () => {
@@ -28,9 +25,9 @@ const NavBar = () => {
 	return (
 		<Navbar expanded={expand} fixed="top" expand="md" className={navColour ? "sticky" : "navbar"}>
 			<Container>
-				{/* <Navbar.Brand href="/">
-					<img src={logo} className="img-fluid logo" alt="brand" />
-				</Navbar.Brand> */}
+				<Navbar.Brand href="/" className="logoType" alt="brand">
+					{"< @ TheLordA />"}
+				</Navbar.Brand>
 				<Navbar.Toggle
 					aria-controls="responsive-navbar-nav"
 					onClick={() => {
@@ -41,7 +38,7 @@ const NavBar = () => {
 					<span></span>
 					<span></span>
 				</Navbar.Toggle>
-				<Navbar.Collapse id="responsive-navbar-nav">
+				<Navbar.Collapse id="responsive-navbar-nav" style={{ flexDirection: " row-reverse" }}>
 					<Nav className="ml-auto" defaultActiveKey="#home">
 						<Nav.Item>
 							<Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
